2a87edfb5061f4cefd6240a29fee9f7cad37f59f
xf23t <jakepark1209@gmail.com>
2016-01-06 16:52:02 +0900

    prrrrr
    ddddd

---
+++ prac/routes/thumbnails.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var mysql = require('mysql');
+var router = express.Router();
+var connection = mysql.createConnection({
+
+    'host': '',
+    'user': '',
+    'password': '',
+    'database': '',
+});
+
+router.get('/', function (req, res, next) {
+    connection.query('select id, title, timestamp from board' + 'order by timestamp desc;', function (error, cursor) {
+        res.json(cursor);
+    });
+});
+
+module.exports=router;
